488aae032f6878de97dfad59cb3e78598d2e77f1
dafalagi <dafarizky34@gmail.com>
2022-05-21 23:11:30 +0700

    update 21/5/22

--- app/Http/Requests/UpdateContentRequest.php
+++ app/Http/Requests/UpdateContentRequest.php
@@ -24,7 +24,14 @@ class UpdateContentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required',
+            'picture' => 'required',
+            'body' => 'required',
+            'excerpt' => 'required',
+            'slug' => 'required|unique:contents',
+            'category' => 'required',
+            'coordinate' => 'required',
+            'trivia' => 'required',
         ];
     }
 }
