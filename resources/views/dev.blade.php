4b826431dd76ce651819c3a8e9e121d9ce9ca6d2
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2022-05-24 20:53:16 +0700

    test

---
+++ resources/views/dev.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com">
+
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
