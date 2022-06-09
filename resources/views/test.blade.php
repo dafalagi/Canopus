5ea31e917eeeb7d9a9fd52acb923be0305752bcc
ImsanInBorderland <67819442+IkhsanNurulRizki@users.noreply.github.com>
2022-06-09 21:17:38 +0700

    modal validasi hapus akun dan lain-lain(selesai)

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,107 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Forum</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;900&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-  <body>
-    <div class="bg-mainclr">
-    @include('.component.navbar')
-      <h1 class="font-bold pb-3 text-center mt-7 mb-7 text-2xl text-white">Selamat datang di Forum Canopus, Astroners!</h1>
-        <div class="flex flex-auto">
-            <div class="lg:ml-7 basis-1/4 sidebar lg:left-0 p-3 overflow-y-auto">
-              <div class="text-gray-900 text-xl">
-                <h1 class="text-white text-lg pl-14 pb-6">Menu</h1>
-                <a href="" class="relative flex items-center">
-                  <img 
-                    class="w-6 absolute ml-4 pointer-events-none" 
-                    src="/imgs/home.png" 
-                    alt=""/>
-                    <span class="p-1 text-lg text-white pl-14 hover:text-secondaryclr">Beranda</span>
-                </a>
-                <a href="" class="relative flex items-center">
-                  <img 
-                    class="w-6 absolute ml-4 pointer-events-none" 
-                    src="/imgs/myTopik.png" 
-                    alt=""/>
-                    <span class="p-1 text-lg text-white pl-14 hover:text-secondaryclr">Topik saya</span>
-                </a>
-                <a href="" class="relative flex items-center">
-                  <img 
-                    class="w-6 absolute ml-4 pointer-events-none" 
-                    src="/imgs/myComment.png" 
-                    alt=""/>
-                    <span class="p-1 text-lg text-white pl-14 hover:text-secondaryclr">Jawaban saya</span>
-                </a>
-              </div>
-              <div class="absolute">
-                <img 
-                    class="w-64  my-24" 
-                    src="/imgs/astronot_2.png" 
-                    alt=""/>
-              </div>
+@include('.component.head')
+<button class="bg-thirdclr hover:opacity-80 px-3 py-1 rounded text-white m-5 show-modal">
+        Hapus Akun
+</button>
+<div class="modal hidden">
+    <div class="h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50">
+        <!-- modal -->
+        <div class="bg-mainclr rounded-xl shadow-lg w-1/3">
+            <!-- modal header -->
+                <div>
+                    <img 
+                        class="w-36 mx-auto m-10" 
+                        src="/imgs/hapusAkun.png" 
+                        alt=""/>
+                </div>
+            <div>
+                <h3 class="text-center font-bold text-white text-2xl">Hapus akun</h3>
             </div>
-            <div class="basis-5/6 w-full pl-64 py-3 bg-grey-50">
-              <form action="">
-                          <div class="relative flex items-center">
-                              <img 
-                                class="w-6 absolute ml-4 pointer-events-none" 
-                                src="/imgs/searchIcon.png" 
-                                alt=""/>
-                          <input 
-                              type="text"
-                              name="search"
-                              placeholder="Cari topik lainnya"
-                              autocomplete="off"
-                              class=" pr-56 pl-12 py-2 font-semibold placeholder-gray-50 text-black rounded-xl border-none ring-2 ring-gray-300 focus:ring-grey-50 focus:ring-2 shadow-lg">
-                          </div>
-                      </form>
+            <!-- modal body -->
+            <div class="mt-3 mr-24 ml-24 text-white text-center text-sm">
+                Apakah kamu yakin ingin menghapus akun kamu?? Ketika kamu menghapus akunmu, kamu tidak akan memiliki akses kembali di Canopus loh.
             </div>
-            <div class=" text-right basis-1/3 w-32 h-10 mr-16">
-              <button
-                    class="px-6 py-5 rounded-lg text-white bg-orange2 bg-opacity-90 hover:bg-secondaryclr shadow-lg">
-                    + Mulai dengan topik baru
-              </button>
-              <div class="absolute">
-                <img 
-                    class="w-64 mx-14 my-16" 
-                    src="/imgs/astronot.png" 
-                    alt=""/>
-              </div>
+            <div class="flex p-12 justify-center">
+                <a href="#" class="bg-white mr-10 px-7 py-2 ring-2 ring-secondaryclr rounded-md text-secondaryclr text-center">
+                    Hapus
+                </a> 
+                <button class=" bg-secondaryclr ring-2 ring-secondaryclr px-7 py-2 rounded-md text-white close-modal text-center">Tutup</button>
             </div>
-          </div>
-        @include('.component.bodyForum')
-        <div class="flex items-center text-white justify-center border-b border-white border-opacity-25 pt-24 pb-9">
-          <div class="flex select-none space-x-1">
-            <a href="#" class="rounded-md bg-gray-200 px-4 py-2 transition duration-200 hover:bg-secondaryclr">
-              <img 
-                class="w-4 absolute -mx-2.5 pointer-events-none" 
-                src="/imgs/leftArrow.png" 
-                alt=""/>
-            </a>
-            <a href="#" class="rounded-md bg-secondaryclr px-4 py-2"> 1 </a>
-            <a href="#" class="rounded-md bg-gray-200 px-4 py-2 transition duration-200 hover:bg-secondaryclr"> 2 </a>
-            <a href="#" class="rounded-md bg-gray-200 px-4 py-2 transition duration-200 hover:bg-secondaryclr"> 3 </a>
-            <span class="rounded-md px-4 py-2 transition duration-200 hover:bg-secondaryclr"> ... </span>
-            <a href="#" class="rounded-md bg-gray-200 px-4 py-2 transition duration-200 hover:bg-secondaryclr"> 10 </a>
-            <a href="#" class="rounded-md bg-gray-200 px-4 py-2 transition duration-200 hover:bg-secondaryclr">
-              <img 
-                class="w-4 absolute -mx-1.5 pointer-events-none" 
-                src="/imgs/rightArrow.png" 
-                alt=""/></a>
-          </div>
-        </div>
         </div>
     </div>
-  </body>
-  @include('component.Footer')
-</html>
+</div>
+<script>
+    const modal = document.querySelector('.modal');
+    
+    const showModal = document.querySelector('.show-modal');
+    const closeModal = document.querySelectorAll('.close-modal');
+    
+    showModal.addEventListener('click', function (){
+      modal.classList.remove('hidden')
+    });
+    
+    closeModal.forEach(close => {
+      close.addEventListener('click', function (){
+        modal.classList.add('hidden')
+      });
+    });
+</script>
