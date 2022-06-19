d2dd5949e4e47d29ccfe989c75fdba0036f0e3d8
arifabdan <arifabdan73@gmail.com>
2022-06-19 20:05:10 +0700

    19/06

--- resources/views/pages/BendaLangit.blade.php
+++ resources/views/pages/BendaLangit.blade.php
@@ -7,6 +7,11 @@
       <div class="flex-col text-left my-80 ml-20">
         <div class="text-8xl font-bold text-white" name="nama_planet">BUMI</div>
         <div class="text-3xl ml-2 italic mt-2 text-orange2" name="julukan_planet">Planet Biru</div>
+        <button class="absolute p-5 bottom-3 right-3 fill-white active:fill-secondaryclr">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 576 512">
+              <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/>
+          </svg>
+      </button>
       </div>
   </div>
   <div class="bg-mainclr bottom-0 text-md p-10">
@@ -66,8 +71,8 @@
        @include('component.cardbendalgt')
     </div>
     <div class="container rounded-lg w-full mx-auto bg-mainclr p-10 mt-10 text-white">
-      <h1 class="text-3xl font-bold underline text-center p-10">Planet Lainnya</h1>
-      <div class="grid grid-cols-3 gap-4 place-items-stretch px-10">
+      <h1 class="text-3xl font-bold underline text-center ">Planet Lainnya</h1>
+      <div class="grid grid-cols-3 gap-4 place-items-stretch px-10 mt-10">
         <div class="relative aspect-video bg-white p-1 rounded-xl">
           <div class="hover:scale-110 transition duration-300 ease-in-out">
             <a href="#">
