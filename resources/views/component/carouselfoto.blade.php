95239117fa379bd3eab848dd00b8174a350cf321
arifabdan <arifabdan73@gmail.com>
2022-07-27 19:37:10 +0700

    Update 27/7

--- resources/views/component/carouselfoto.blade.php
+++ resources/views/component/carouselfoto.blade.php
@@ -1,29 +1,69 @@
-<div id="animation-carousel" class="relative" data-carousel="static">
-    <div class="overflow-hidden relative h-48 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-        @if ($content->pictures)
-            @foreach ($content->pictures as $picture)
-                <div class="hidden duration-200 ease-linear" data-carousel-item>
-                    <img src="{{ asset('storage/'.$picture) }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
-                </div>
-            @endforeach
-        @else
-            <div class="hidden duration-200 ease-linear" data-carousel-item>
-                <img src="https://source.unsplash.com/640x480?space" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
-            </div>
-        @endif
+<!Grid Satu Foto>
+<div class="flex flex-wrap justify-center">
+  <div class="w-6/12 sm:w-4/12 px-4 rounded-xl">
+      <div class="hover:scale-110 transition duration-300 ease-in-out">
+        <a href="">
+        <img src="https://source.unsplash.com/640x480?space" alt="..." class="shadow rounded-xl max-w-full h-auto align-middle border-none" />
+        </a>
+      </div>
+  </div>
+</div>
+<!Grid Banyak Foto>
+<div class="container px-6 lg:pt-5 lg:px-32">
+    <div class="flex flex-wrap -m-1 md:-m-2">
+      <div class="flex flex-wrap w-1/3">
+        <div class="hover:scale-110 transition duration-300 ease-in-out">
+          <div class="w-full p-1 md:p-2">
+            <a href="">
+              <img src="https://source.unsplash.com/640x480?space" alt="..." class="shadow rounded-xl max-w-full h-auto align-middle border-none" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/3">
+        <div class="hover:scale-110 transition duration-300 ease-in-out">
+          <div class="w-full p-1 md:p-2">
+            <a href="">
+              <img src="https://source.unsplash.com/640x480?space" alt="..." class="shadow rounded-xl max-w-full h-auto align-middle border-none" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/3">
+        <div class="hover:scale-110 transition duration-300 ease-in-out">
+          <div class="w-full p-1 md:p-2">
+            <a href="">
+              <img src="https://source.unsplash.com/640x480?space" alt="..." class="shadow rounded-xl max-w-full h-auto align-middle border-none" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/3">
+        <div class="hover:scale-110 transition duration-300 ease-in-out">
+          <div class="w-full p-1 md:p-2">
+            <a href="">
+              <img src="https://source.unsplash.com/640x480?space" alt="..." class="shadow rounded-xl max-w-full h-auto align-middle border-none" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/3">
+        <div class="hover:scale-110 transition duration-300 ease-in-out">
+          <div class="w-full p-1 md:p-2">
+            <a href="">
+              <img src="https://source.unsplash.com/640x480?space" alt="..." class="shadow rounded-xl max-w-full h-auto align-middle border-none" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/3">
+        <div class="hover:scale-110 transition duration-300 ease-in-out">
+          <div class="w-full p-1 md:p-2">
+            <a href="">
+              <img src="https://source.unsplash.com/640x480?space" alt="..." class="shadow rounded-xl max-w-full h-auto align-middle border-none" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-    <button type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10">
-        <svg class="h-10 w-10 fill-white"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z"/>
-        </svg>
-            <span class="hidden">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
-    <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10">
-        <svg class="h-10 w-10 fill-white"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z"/>
-        </svg>
-            <span class="hidden">Next</span>
-        </span>
-    </button>
 </div>
--- resources/views/pages/content.blade.php
+++ resources/views/pages/content.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   @include('component.head')
-  <body class="">
+  <body class="font-poppins">
   <div class="h-screen w-full pt-1 bg-no-repeat bg-left-top"
     @if($content->mainpicture)
       style="background-image: url({{ asset('storage/'.$content->mainpicture) }})"
@@ -11,8 +11,7 @@
     >
     @include('component.navbar');
       <div class="flex-col text-left my-80 ml-20">
-        <div class="text-8xl font-bold text-white font-poppins" name="nama_planet">{{ $content->title }}</div>
-        <div class="text-3xl ml-2 italic mt-2 text-orange2 font-poppins" name="julukan_planet">Planet Biru</div>
+        <div class="text-8xl font-bold text-white" name="nama_planet">{{ $content->title }}</div>
         @php
             if(auth()->user())
             {
@@ -23,7 +22,7 @@
         @if (auth()->user() && $favorite->isNotEmpty() && $favorite->first()->content_id == $content->id)
           <form action="/favorites/delete/{{ $favorite->first()->id }}" method="post">
               @csrf
-              <button class="absolute p-5 bottom-3 right-3 fill-secondaryclr">
+              <button class="absolute p-5 bottom-3 right-3 fill-secondaryclr" data-modal-toggle="alert-succ">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 576 512">
                       <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/>
                   </svg>
@@ -44,12 +43,12 @@
   <div class="bg-mainclr bottom-0 text-md p-10">
       <div class="flex justify-center font-bold text-2xl">
         <div class="flex-col text-center mx-20">
-          <div class="text-orange2 font-poppins">Jarak Dari Bumi</div>
-          <div class="text-grey font-poppins" name="jrk_matahari">{{ $content->distance }}</div>
+          <div class="text-orange2">Jarak Dari Bumi</div>
+          <div class="text-grey" name="jrk_matahari">{{ $content->distance }}</div>
         </div>
         <div class="flex-col text-center mx-20">
-          <div class="text-orange2 font-poppins">Koordinat</div>
-          <div class="text-grey font-poppins" name="koordinat">{{ $content->coordinate }}</div>
+          <div class="text-orange2">Koordinat</div>
+          <div class="text-grey" name="koordinat">{{ $content->coordinate }}</div>
         </div>
       </div>
   </div>
@@ -59,35 +58,33 @@
       <div class="flex text-white mt-10 ml-12">
         <div class="basis-full">
         <div class="flex-col text-justify">
-          <div class="text-2xl font-bold font-poppins">Apa itu {{ $content->title }}?</div>
-          <div class="mt-3 font-poppins" name="desc">{!! $content->intro !!}
+          <div class="text-2xl font-bold">Apa itu {{ $content->title }}?</div>
+          <div class="mt-3" name="desc">{!! $content->intro !!}
           </div>
-            <div class="text-2xl font-bold pt-5 font-poppins">Sejarah {{ $content->title }}</div>
-              <div class="mt-3 font-poppins" name="desc">{!! $content->history !!}  
+            <div class="text-2xl font-bold pt-5">Sejarah {{ $content->title }}</div>
+              <div class="mt-3" name="desc">{!! $content->history !!}  
               </div>
         </div>
         </div>
         <div class="basis-2/4">
           <div class="flex-col text-center mx-20">
-            <div class="text-2xl font-bold font-poppins">Foto</div>
-            <div class="pt-0 rounded-lg">
-            @include('component.carouselfoto')
-            </div>
             <div class="pt-5">
               <img class="w-auto" src="/imgs/Astro.png" alt="">
             </div>
           </div>
         </div>
       </div>
+      <h1 class="text-2xl py-5 font-bold underline text-center">Foto</h1>
+      @include('component.carouselfoto')
         @if($events->first() != null)
           @include('component.cardbendalgt')
         @endif
     </div>
     <div class="container rounded-lg w-full mx-auto bg-mainclr p-10 mt-10 text-white">
       @if ($content->category == 'Lainnya di Angkasa')
-        <h1 class="text-3xl font-bold underline text-center font-poppins">Lainnya di Angkasa</h1>
+        <h1 class="text-3xl font-bold underline text-center">Lainnya di Angkasa</h1>
       @else
-        <h1 class="text-3xl font-bold underline text-center font-poppins">{{ $content->category }} Lainnya</h1>
+        <h1 class="text-3xl font-bold underline text-center">{{ $content->category }} Lainnya</h1>
       @endif
       <div class="grid grid-cols-3 gap-4 place-items-stretch px-10 mt-10">
         @foreach ($others->except($content->id)->take(6) as $content)
