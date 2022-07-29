b1191400f4992ff7f9a8cdefd8297eea8af769d8
dafalagi <dafarizky34@gmail.com>
2022-07-29 22:51:12 +0700

    update 29/7/22

--- resources/views/component/bodyForum.blade.php
+++ resources/views/component/bodyForum.blade.php
@@ -20,7 +20,7 @@
                 @else
                   <form action="/favorites/discuss/{{ $discuss->slug }}" method="POST">
                     @csrf
-                    <button class="border-b flex pl-2 py-2 left-0 rounded-t-lg w-full text-white hover:bg-thirdclr dark:hover:bg-gray-600">
+                    <button class="border-b flex pl-3 py-2 left-0 rounded-t-lg w-full text-white hover:bg-thirdclr dark:hover:bg-gray-600">
                       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2 fill-white" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M384 48V512l-192-112L0 512V48C0 21.5 21.5 0 48 0h288C362.5 0 384 21.5 384 48z"/></svg>
                         Simpan    
                     </button>
@@ -28,7 +28,7 @@
                 @endif
               </li>
               <li>
-                <button class="flex pl-2 py-2 rounded-b-lg left-0 w-full show-modal text-white hover:bg-thirdclr dark:hover:bg-gray-600" type="button" data-modal-toggle="authentication-modal">
+                <button class="flex pl-3 py-2 rounded-b-lg left-0 w-full show-modal text-white hover:bg-thirdclr dark:hover:bg-gray-600" type="button" data-modal-toggle="authentication-modal">
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2 fill-white" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM232 152C232 138.8 242.8 128 256 128s24 10.75 24 24v128c0 13.25-10.75 24-24 24S232 293.3 232 280V152zM256 400c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 385.9 273.4 400 256 400z"/></svg>
                     Laporkan
                 </button>
