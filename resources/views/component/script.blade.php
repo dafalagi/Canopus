446e37873c2bdd8b9bbf26d23c94549665ab494f
dafalagi <dafarizky34@gmail.com>
2022-08-07 22:05:32 +0700

    update 7/8/22

--- resources/views/component/script.blade.php
+++ resources/views/component/script.blade.php
@@ -1,8 +1,8 @@
-<script src="../path/to/flowbite/dist/flowbite.js"></script>
-<script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
+{{-- <script src="../path/to/flowbite/dist/flowbite.js"></script> --}}
+<script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
 
 {{-- <script>
     var tag = document.createElement("script");
-    tag.src = "https://unpkg.com/flowbite@1.4.7/dist/flowbite.js";
+    tag.src = "https://unpkg.com/flowbite@1.5.1/dist/flowbite.js";
     document.getElementsByTagName("head")[0].appendChild(tag);
 </script> --}}
