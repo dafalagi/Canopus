cc96ecca17ea27ded3c6e8b4dd36e71e988618c7
dafalagi <dafarizky34@gmail.com>
2022-08-07 15:45:54 +0700

    update 7/8/22

--- resources/views/component/cardcommentlv2.blade.php
+++ resources/views/component/cardcommentlv2.blade.php
@@ -8,11 +8,11 @@
     <div class="w-fit">
       <div class="relative bg-mainclr ml-10 mt-4 grid grid-cols-1 rounded-md py-2 px-4">
       {{-- ini dropdown abawah --}}  
-      <button id="menu-btn3" data-dropdown-toggle="dropdown3-{{ $comment->id }}" class="absolute right-0 top-0 text-white hover:bg-thirdclr font-medium rounded-md text-sm px-2.5 py-2.5 text-center inline-flex items-center " type="button">
+      <button id="menu-btn3" data-dropdown-toggle="dropdown3-{{ $reply->id }}" class="absolute right-0 top-0 text-white hover:bg-thirdclr font-medium rounded-md text-sm px-2.5 py-2.5 text-center inline-flex items-center " type="button">
         <svg class="w-5" fill="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
       <!-- Dropdown menu bawah -->
-        <div id="dropdown3-{{ $comment->id }}" class=" hidden z-10 rounded divide-y shadow" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="bottom" style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(246.667px, 70px, 0px);">
+        <div id="dropdown3-{{ $reply->id }}" class=" hidden z-10 rounded divide-y shadow" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="bottom" style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(246.667px, 70px, 0px);">
           <ul class="text-sm text-white bg-mainclr rounded-md" aria-labelledby="dropdownDefault">
             @if (auth()->user() && $reply->user_id == auth()->user()->id)
               <li>
@@ -25,7 +25,7 @@
               </li>
               <li>
                 <form action="" method="POST" class="block py-2 px-4 hover:bg-thirdclr hover:text-secondaryclr">
-                  <button type="button" data-modal-toggle="deleteComment-modal-{{ $comment->id }}">  
+                  <button type="button" data-modal-toggle="deleteComment-modal-{{ $reply->id }}">  
                     <svg class="w-4 absolute fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z"/></svg>  
                     <span class="ml-6">Hapus Komentar</span>
                   </button>   
@@ -34,7 +34,7 @@
             @endif
             <li>
               <form action="" method="POST" class="block py-2 px-4 hover:bg-thirdclr hover:rounded-b-md hover:text-secondaryclr">
-                <button type="button" data-modal-toggle="authentication2-modal">  
+                <button type="button" data-modal-toggle="authentication2-modal-{{ $reply->id }}">  
                   <svg class="w-4 absolute fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM232 152C232 138.8 242.8 128 256 128s24 10.75 24 24v128c0 13.25-10.75 24-24 24S232 293.3 232 280V152zM256 400c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 385.9 273.4 400 256 400z"/></svg>
                   <span class="ml-6">Laporkan</span>
                 </button>   
